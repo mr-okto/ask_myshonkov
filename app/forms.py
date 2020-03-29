c417181ba3213765d069a159ae1a12039de751c5
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2020-03-29 22:52:10 +0300

    Add question page

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,17 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.core import validators
 import re
 
 
 class AskForm(forms.Form):
-    title = forms.CharField(
-        max_length=200,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    text = forms.CharField(
-        max_length=500,
-        widget=forms.Textarea(attrs={'class': 'form-control'})
-    )
-    tags = forms.CharField(
-        max_length=100,
-        required=False,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
+    title = forms.CharField(max_length=140,
+                            widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    text = forms.CharField(max_length=1000,
+                           widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+    tags = forms.CharField(max_length=100, required=False,
+                           widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     def clean_tags(self):
         tags = [tag.strip() for tag in self.cleaned_data['tags'].split(',')]
@@ -30,3 +24,13 @@ class AskForm(forms.Form):
                 raise forms.ValidationError('Tags must consist of letters, numbers and underscores. '
                                             'Use comma as a separator')
         return tags
+
+
+class AnswerForm(forms.Form):
+    text = forms.CharField(max_length=1000,
+                           widget=forms.Textarea(attrs={'class': 'form-control',
+                                                        'placeholder': 'Enter your answer here...'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].label = ''
--- app/views.py
+++ app/views.py
@@ -5,24 +5,33 @@ from django.core.paginator import Paginator
 
 from .forms import *
 
-questions = [{'id': 0, 'title': f'question # {0}', 'votes': 544,
+answers = [{'pk': 1, 'votes': 5, 'author': 'Nobody', 'correct': True,
+           'text': 'Автор вопроса может пометить один из ответов как правильный.'
+                   ' Пользователи могут голосовать за вопросы и ответы с помощью '
+                   'лайков. '},
+           {'pk': 2, 'votes': 2, 'author': 'Noone', 'correct': False,
+            'text': 'This works because returning false from the click event'
+                    ' stops the chain of execution continuing.'}]
+
+questions = [{'id': 0, 'title': f'question # {0}', 'votes': 544, 'author': 'Guest',
               'text': 'Листинг вопросов с пагинацией по 20 вопросов на странице. '
                       'Необходимо реализовать сортировку по дате добавления и рейтингу'
                       ' (2 вида сортировки). В шапке сайта находятся: логотип, поисковая'
                       ' строка (для быстрого поиска по заголовку и содержимому вопроса), '
                       'кнопка задать вопрос (доступна только авторизованным). '
                       'В правой части шапки - юзерблок. ',
-              'answers_count': 10,
+              'answers_count': 35,
               'tags': ['tag1', 'tag2', 'tag3']},
-             {'id': 1, 'title': f'question # {1}', 'votes': 2,
+             {'id': 1, 'title': f'question # {1}', 'votes': 2, 'author': 'Kotk',
               'text': 'Для авторизованного пользователя юзерблок содержит его ник',
-              'answers_count': 0,
+              'answers_count': 3,
               'tags': ['tag1', 'tag2']}]
+
 questions *= 30
 
 
-def paginate(request, objects):
-    paginator = Paginator(objects, 10)
+def paginate(request, objects, page_count):
+    paginator = Paginator(objects, page_count)
     page_ind = request.GET.get('page', 1)
     page = paginator.get_page(page_ind)
     return page
@@ -33,14 +42,14 @@ def index(request):
         'title': 'New questions',
         'switch_title': 'Hot questions',
         'switch_url': 'index',
-        'questions': paginate(request, questions)
+        'questions': paginate(request, questions, 10)
     })
 
 
-# @login_required(login_url='/login')
+# @login_required()
 def ask(request):
-    form = AskForm(request.POST)
     if request.method == 'POST':
+        form = AskForm(request.POST)
         if form.is_valid():
             tags = form.cleaned_data.get('tags')
     else:
@@ -48,10 +57,20 @@ def ask(request):
     return render(request, 'ask.html', {'form': form})
 
 
-def login(reqest):
+def login(request):
     return HttpResponse('Login Page')
 
 
 def question(request, qid):
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data.get('text')
+    else:
+        form = AnswerForm()
+
     q = questions[qid]
-    return render(request, 'question_preview.html', {'question': q})
+    ans = paginate(request, answers, 30)
+    return render(request, 'question.html',
+                  {'question': q, 'answers': ans, 'form': form})
