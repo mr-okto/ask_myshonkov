f8a6000e65ffaad44a378b158375d026b5f743d5
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2020-03-30 22:04:01 +0300

    Config PostgreSQL

--- askme/settings.py
+++ askme/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'askme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'askme_db',
+        'USER': 'db_user',
+        'PASSWORD': 'db_pass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
